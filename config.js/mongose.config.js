6ec7438b1ab5dd6aaec6ff8b7e44d26fc7339dd4
ankit <ankitchakraborty@30@gmail.com>
2024-10-03 22:22:19 +0530

    code updated

--- config.js/mongose.config.js
+++ config.js/mongose.config.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-const Port='mongodb://localhost:27017/Habits'
+const Port = "mongodb://localhost:27017/Habits";
 
 const connect = async () => {
-  await mongoose.connect(Port);
-  console.log("mongose is connected");
+  await mongoose.connect(Port, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connect;
