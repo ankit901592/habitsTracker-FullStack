688d59b031e45146bc897675f225cc939ac46d89
ankit <ankitchakraborty@30@gmail.com>
2024-10-05 14:18:45 +0530

    code updated

--- config.js/mongose.config.js
+++ config.js/mongose.config.js
@@ -1,10 +1,17 @@
+
+
 import mongoose from "mongoose";
-// const Port = "mongodb://localhost:27017/Habits";
 
 const connect = async () => {
-  await mongoose.connect( process.env.MONGO_URI);
-  console.log('mongodb connected');
-  
+  mongoose.connect(process.env.MONGO_URI, {
+    ssl: true,
+    tlsInsecure: false,
+  }).then(() => {
+    console.log("Connected to MongoDB with TLS");
+  }).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  })
 };
 
 export default connect;
+
